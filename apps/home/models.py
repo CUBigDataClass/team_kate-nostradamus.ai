5d9273b6b2c049328a7c42ef866b0ecdff102b48
LuisRCorzo <53102243+LuisRCorzo@users.noreply.github.com>
2022-04-25 21:52:51 -0700

    Updated database structure for predictions.

--- apps/home/models.py
+++ apps/home/models.py
@@ -4,7 +4,7 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from datetime import datetime
-
+import time
 import json
 
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -56,8 +56,21 @@ class BTC(db.Model):
 class BTC_forecasts(db.Model):
 
     __tablename__ = 'btc_predictions'
-    #time = db.Column(db.Float(), primary_key=True)
-    close = db.Column(db.Float(), primary_key=True)
+    time = db.Column(db.Float(), primary_key=True)
+    close_1 = db.Column(db.Float())
+    close_2 = db.Column(db.Float())
+    close_3 = db.Column(db.Float())
+    close_4 = db.Column(db.Float())
+    close_5 = db.Column(db.Float())
+    close_6 = db.Column(db.Float())
+    close_7 = db.Column(db.Float())
+    close_8 = db.Column(db.Float())
+    close_9 = db.Column(db.Float())
+    close_10 = db.Column(db.Float())
+    close_11 = db.Column(db.Float())
+    close_12 = db.Column(db.Float())
+    close_13 = db.Column(db.Float())
+    close_14 = db.Column(db.Float())
 
     @classmethod
     def get_historical(self):
@@ -98,11 +111,13 @@ def create_BTC_forecasts(*args, **kwargs):
     btc=BTC.query.all()
     btc=pd.DataFrame(BTC.toDICT(btc))
     btc_preds = get_predictions(btc)
+    curr_date = datetime.now()
+    curr_time = time.mktime(curr_date.timetuple())
     
-    for days in btc_preds:
-        row=BTC_forecasts(close=days)
-        db.session.add(row)
-        db.session.commit()
+    row=BTC_forecasts(time=curr_time, close_1=btc_preds[0], close_2=btc_preds[1], close_3=btc_preds[2], close_4=btc_preds[3], close_5=btc_preds[4], close_6=btc_preds[5], close_7=btc_preds[6], close_8=btc_preds[7], close_9=btc_preds[8], close_10=btc_preds[9], close_11=btc_preds[10], close_12=btc_preds[11], close_13=btc_preds[12], close_14=btc_preds[13])
+    
+    db.session.add(row)
+    db.session.commit()
 
 
 
@@ -140,7 +155,21 @@ class ETH(db.Model):
 class ETH_forecasts(db.Model):
 
     __tablename__ = 'eth_predictions'
-    close = db.Column(db.Float(), primary_key=True)
+    time = db.Column(db.Float(), primary_key=True)
+    close_1 = db.Column(db.Float())
+    close_2 = db.Column(db.Float())
+    close_3 = db.Column(db.Float())
+    close_4 = db.Column(db.Float())
+    close_5 = db.Column(db.Float())
+    close_6 = db.Column(db.Float())
+    close_7 = db.Column(db.Float())
+    close_8 = db.Column(db.Float())
+    close_9 = db.Column(db.Float())
+    close_10 = db.Column(db.Float())
+    close_11 = db.Column(db.Float())
+    close_12 = db.Column(db.Float())
+    close_13 = db.Column(db.Float())
+    close_14 = db.Column(db.Float())
 
     @classmethod
     def get_historical(self):
@@ -185,11 +214,13 @@ def create_ETH_forecasts(*args, **kwargs):
     eth=pd.DataFrame(ETH.toDICT(eth))
 
     eth_preds = get_predictions(eth)
-
-    for days in eth_preds:
-        row=ETH_forecasts(close=days)
-        db.session.add(row)
-        db.session.commit()
+    curr_date = datetime.now()
+    curr_time = time.mktime(curr_date.timetuple())
+    
+    row=ETH_forecasts(time=curr_time, close_1=eth_preds[0], close_2=eth_preds[1], close_3=eth_preds[2], close_4=eth_preds[3], close_5=eth_preds[4], close_6=eth_preds[5], close_7=eth_preds[6], close_8=eth_preds[7], close_9=eth_preds[8], close_10=eth_preds[9], close_11=eth_preds[10], close_12=eth_preds[11], close_13=eth_preds[12], close_14=eth_preds[13])
+    
+    db.session.add(row)
+    db.session.commit()
 
 
 class XMR(db.Model):
@@ -225,7 +256,22 @@ class XMR(db.Model):
 class XMR_forecasts(db.Model):
 
     __tablename__ = 'xmr_predictions'
-    close = db.Column(db.Float(), primary_key=True)
+
+    time = db.Column(db.Float(), primary_key=True)
+    close_1 = db.Column(db.Float())
+    close_2 = db.Column(db.Float())
+    close_3 = db.Column(db.Float())
+    close_4 = db.Column(db.Float())
+    close_5 = db.Column(db.Float())
+    close_6 = db.Column(db.Float())
+    close_7 = db.Column(db.Float())
+    close_8 = db.Column(db.Float())
+    close_9 = db.Column(db.Float())
+    close_10 = db.Column(db.Float())
+    close_11 = db.Column(db.Float())
+    close_12 = db.Column(db.Float())
+    close_13 = db.Column(db.Float())
+    close_14 = db.Column(db.Float())
 
     @classmethod
     def get_historical(self):
@@ -269,11 +315,12 @@ def create_XMR_forecasts(*args, **kwargs):
     xmr=pd.DataFrame(XMR.toDICT(xmr))
 
     xmr_preds = get_predictions(xmr)
-
-    for days in xmr_preds:
-        row=XMR_forecasts(close=days)
-        db.session.add(row)
-        db.session.commit()
+    curr_date = datetime.now()
+    curr_time = time.mktime(curr_date.timetuple())
+    
+    row=XMR_forecasts(time=curr_time, close_1=xmr_preds[0], close_2=xmr_preds[1], close_3=xmr_preds[2], close_4=xmr_preds[3], close_5=xmr_preds[4], close_6=xmr_preds[5], close_7=xmr_preds[6], close_8=xmr_preds[7], close_9=xmr_preds[8], close_10=xmr_preds[9], close_11=xmr_preds[10], close_12=xmr_preds[11], close_13=xmr_preds[12], close_14=xmr_preds[13])
+    db.session.add(row)
+    db.session.commit()
 
 # <<<<<<< Updated upstream
 # # @scheduler.task('interval', id='update_daily_values', seconds=5)
@@ -324,39 +371,34 @@ def daily_db_update():
             pass 
 
 
-@scheduler.task('interval', id='make_weekly_predictions', weeks=1)
-def daily_db_update():
+@scheduler.task('interval', id='make_biweekly_predictions', weeks=2)
+def biweekly_db_update():
 
     with scheduler.app.app_context():
+
+        curr_date = datetime.now()
+        curr_time = time.mktime(curr_date.timetuple())
+
         btc=BTC.query.all()
         btc=pd.DataFrame(BTC.toDICT(btc))
         btc_preds = get_predictions(btc)
-        
-        for days in btc_preds:
-            row=BTC_forecasts(close=days)
-            db.session.add(row)
-            db.session.commit()
+        row1=BTC_forecasts(time=curr_time, close_1=btc_preds[0], close_2=btc_preds[1], close_3=btc_preds[2], close_4=btc_preds[3], close_5=btc_preds[4], close_6=btc_preds[5], close_7=btc_preds[6], close_8=btc_preds[7], close_9=btc_preds[8], close_10=btc_preds[9], close_11=btc_preds[10], close_12=btc_preds[11], close_13=btc_preds[12], close_14=btc_preds[13])
+        db.session.add(row1)
+        db.session.commit()
         
         eth=ETH.query.all()
         eth=pd.DataFrame(ETH.toDICT(eth))
-
         eth_preds = get_predictions(eth)
-
-        for days in eth_preds:
-            row=ETH_forecasts(close=days)
-            db.session.add(row)
-            db.session.commit()
-
+        row2=ETH_forecasts(time=curr_time, close_1=eth_preds[0], close_2=eth_preds[1], close_3=eth_preds[2], close_4=eth_preds[3], close_5=eth_preds[4], close_6=eth_preds[5], close_7=eth_preds[6], close_8=eth_preds[7], close_9=eth_preds[8], close_10=eth_preds[9], close_11=eth_preds[10], close_12=eth_preds[11], close_13=eth_preds[12], close_14=eth_preds[13])
+        db.session.add(row2)
+        db.session.commit()
 
         xmr=XMR.query.all()
         xmr=pd.DataFrame(XMR.toDICT(xmr))
-
         xmr_preds = get_predictions(xmr)
-
-        for days in xmr_preds:
-            row=XMR_forecasts(close=days)
-            db.session.add(row)
-            db.session.commit()
+        row3=XMR_forecasts(time=curr_time, close_1=xmr_preds[0], close_2=xmr_preds[1], close_3=xmr_preds[2], close_4=xmr_preds[3], close_5=xmr_preds[4], close_6=xmr_preds[5], close_7=xmr_preds[6], close_8=xmr_preds[7], close_9=xmr_preds[8], close_10=xmr_preds[9], close_11=xmr_preds[10], close_12=xmr_preds[11], close_13=xmr_preds[12], close_14=xmr_preds[13])
+        db.session.add(row3)
+        db.session.commit()
 
 def toDICT(rset):
     result = defaultdict(list)
@@ -381,10 +423,12 @@ def get_eth_data():
 
     eth=ETH.query.all()
     eth_pred=ETH_forecasts.query.all()
-
-
+    
     eth_df = pd.DataFrame(ETH.toDICT(eth))
-    start = len(eth_df)-45
+    eth_pred= pd.DataFrame(ETH.toDICT(eth_pred))
+    
+    
+    start = len(eth_df)-70
 
     eth_df = eth_df[['close']]
     eth_actual = (eth_df.iloc[start:,].values.ravel()).tolist()
@@ -398,16 +442,33 @@ def get_eth_data():
     
     
     
-    idx_pred = [i for i in range(len(eth_actual), len(eth_actual)+len(eth_pred))]
+    idx_pred = [i for i in range(len(eth_actual), len(eth_actual)+14)]
     json_preds = []
     json_preds.append(json_actuals[-1])
     
+    close1 = eth_pred.close_1.values
+    close2 = eth_pred.close_2.values
+    close3 = eth_pred.close_3.values
+    close4 = eth_pred.close_4.values
+    close5 = eth_pred.close_5.values
+    close6 = eth_pred.close_6.values
+    close7 = eth_pred.close_7.values
+    close8 = eth_pred.close_8.values
+    close9 = eth_pred.close_9.values
+    close10 = eth_pred.close_10.values
+    close11 = eth_pred.close_11.values
+    close12 = eth_pred.close_12.values
+    close13 = eth_pred.close_13.values
+    close14 = eth_pred.close_14.values
 
-    for pred,idx in zip(eth_pred, idx_pred):
-
-        json_preds.append({'x':idx ,'y':pred.close })
-
+    preds = [close1[0],close2[0], close3[0], close4[0], close5[0], close6[0], close7[0], close8[0], close9[0], close10[0], close11[0], close12[0], close13[0], close14[0]]
     
+    for pred,idx in zip(preds, idx_pred):
+
+        json_preds.append({'x':idx ,'y':pred})
+
+    print(json_actuals)
+    print(json_preds)
     return json.dumps(json_actuals),json.dumps(json_preds)
 
 def get_btc_data():
@@ -417,7 +478,9 @@ def get_btc_data():
 
 
     btc_df = pd.DataFrame(BTC.toDICT(btc))
-    start = len(btc_df)-45
+    btc_pred = pd.DataFrame(ETH.toDICT(btc_pred))
+
+    start = len(btc_df)-70
 
     btc_df = btc_df[['close']]
     btc_actual = (btc_df.iloc[start:,].values.ravel()).tolist()
@@ -431,15 +494,33 @@ def get_btc_data():
     
     
     
-    idx_pred = [i for i in range(len(btc_actual), len(btc_actual)+len(btc_pred))]
+    idx_pred = [i for i in range(len(btc_actual), len(btc_actual)+14)]
     json_preds = []
     json_preds.append(json_actuals[-1])
     
 
-    for pred,idx in zip(btc_pred, idx_pred):
+    close1 = btc_pred.close_1.values
+    close2 = btc_pred.close_2.values
+    close3 = btc_pred.close_3.values
+    close4 = btc_pred.close_4.values
+    close5 = btc_pred.close_5.values
+    close6 = btc_pred.close_6.values
+    close7 = btc_pred.close_7.values
+    close8 = btc_pred.close_8.values
+    close9 = btc_pred.close_9.values
+    close10 = btc_pred.close_10.values
+    close11 = btc_pred.close_11.values
+    close12 = btc_pred.close_12.values
+    close13 = btc_pred.close_13.values
+    close14 = btc_pred.close_14.values
 
-        json_preds.append({'x':idx ,'y':pred.close })
+    preds = [close1[0],close2[0], close3[0], close4[0], close5[0], close6[0], close7[0], close8[0], close9[0], close10[0], close11[0], close12[0], close13[0], close14[0]]
+    
+    for pred,idx in zip(preds, idx_pred):
 
+        json_preds.append({'x':idx ,'y':pred})
+
+    
   
     return json.dumps(json_actuals),json.dumps(json_preds)
 
@@ -448,9 +529,11 @@ def get_xmr_data():
     xmr=XMR.query.all()
     xmr_pred=XMR_forecasts.query.all()
 
-
     xmr_df = pd.DataFrame(XMR.toDICT(xmr))
-    start = len(xmr_df)-45
+    xmr_pred = pd.DataFrame(ETH.toDICT(xmr_pred))
+
+
+    start = len(xmr_df)-70
 
     xmr_df =  xmr_df[['close']]
     xmr_actual = (xmr_df.iloc[start:,].values.ravel()).tolist()
@@ -464,16 +547,34 @@ def get_xmr_data():
     
     
     
-    idx_pred = [i for i in range(len(xmr_actual), len(xmr_actual)+len(xmr_pred))]
+    idx_pred = [i for i in range(len(xmr_actual), len(xmr_actual)+14)]
     json_preds = []
     json_preds.append(json_actuals[-1])
     
 
-    for pred,idx in zip(xmr_pred, idx_pred):
+    close1 = xmr_pred.close_1.values
+    close2 = xmr_pred.close_2.values
+    close3 = xmr_pred.close_3.values
+    close4 = xmr_pred.close_4.values
+    close5 = xmr_pred.close_5.values
+    close6 = xmr_pred.close_6.values
+    close7 = xmr_pred.close_7.values
+    close8 = xmr_pred.close_8.values
+    close9 = xmr_pred.close_9.values
+    close10 = xmr_pred.close_10.values
+    close11 = xmr_pred.close_11.values
+    close12 = xmr_pred.close_12.values
+    close13 = xmr_pred.close_13.values
+    close14 = xmr_pred.close_14.values
 
-        json_preds.append({'x':idx ,'y':pred.close })
+    preds = [close1[0],close2[0], close3[0], close4[0], close5[0], close6[0], close7[0], close8[0], close9[0], close10[0], close11[0], close12[0], close13[0], close14[0]]
+    
 
-    # print(json_preds)
+    for pred,idx in zip(preds, idx_pred):
+
+        json_preds.append({'x':idx ,'y':pred})
+
+    
     return json.dumps(json_actuals),json.dumps(json_preds)
 
 def get_predictions(df):
@@ -555,86 +656,95 @@ def get_predictions(df):
 
 
 
-    in_window = 70
-    out_window = 14
-    test_len = out_window
-
+    in_window = 70 
+    out_window = 14 
+    test_len = 0 
 
     num_features = len(data_df.columns)
+
     dataset_len = len(data_df)
     train_len = len(data_df)-test_len
 
+    #last_seen_close = close_df[train_len]
 
-    data = data_df.iloc[:,0:num_features].values
-    close = data_df[['close']].values       
 
+    data = data_df.iloc[:,0:num_features].values       
 
     #____________________________________________
     #---------- standard scaling ----------------
+
     sc = StandardScaler()
     sc2 = StandardScaler()
-
 
     train_data = sc.fit_transform(data[:train_len,:])
     train_close = sc2.fit_transform(np.asarray(close[:train_len]).reshape(-1,1))
 
-    #_________________________________________________________________________________________
-    #___________Train/Validation Data:________________________________________________________
+    #plt.hist(train_data,bins=30)
+
+    #____________________________________________
+    #___________Train/Validation Data:___________
 
     x_train = []
     y_train = []
 
+
     x_valid = []
     y_valid = []
 
-
-    for i in range(in_window,len(train_data)-out_window-out_window+1):
+    for i in range(in_window,len(train_data)-out_window+1):
         
         x_train.append(train_data[i-in_window:i,:])
-        y_train.append(train_close[i:i+out_window,:])
-        
-        x_valid.append(train_data[i-in_window+out_window:i+out_window,:])
-        y_valid.append(train_close[i+out_window:i+out_window+out_window,:])
-        
+        y_train.append(train_close[i:i+out_window])
+    
+        if i < len(train_data)-out_window-out_window+1:
+            x_valid.append(train_data[i-in_window+out_window:i+out_window,:])
+            y_valid.append(train_close[i+out_window:i+out_window+out_window])
+
         
 
     #______reshape train-data to lstm expected shape_______
 
-    x_train = np.array(x_train)
-    y_train = np.array(y_train)
+    x_train = np.array(x_train).astype('float32')
+    y_train = np.array(y_train).astype('float32')
 
     x_train = np.reshape(x_train,(x_train.shape[0],x_train.shape[1],num_features))
 
-    x_valid = np.array(x_valid)
-    y_valid = np.array(y_valid)
+    print(x_train.shape,y_train.shape)
+
+    x_valid = np.array(x_valid).astype('float32')
+    y_valid = np.array(y_valid).astype('float32')
 
     x_valid = np.reshape(x_valid,(x_valid.shape[0],x_valid.shape[1],num_features))
 
-    #_________________________________________________________________________________________
-    #______________Test Data:_________________________________________________________________
+    print(x_valid.shape,y_valid.shape)
 
 
-    #--------- standard-scaling ----------
+    #______________Test Data:____________________
+
+    #____________________________________________
+    #--------- standard-scaling (test) ----------
 
     test_data = sc.transform(data[train_len-in_window:,:])
-    test_close = sc2.transform(np.asarray(close[train_len-in_window:,:]).reshape(-1,1))
+
+    #test_close = sc2.transform(np.asarray(close[train_len-in_window:]).reshape(-1,1))
+
+    #____________________________________________
+    #--------- log-transformed (test) -----------
+
+
+    #____________________________________________
 
     x_test = []
-    y_test = []
+    #y_test = []
 
 
-    for i in range(in_window,len(test_data)-out_window+1):
-        
-        x_test.append(test_data[i-in_window:i,:])
-        y_test.append(test_close[i:i+out_window,:])
+    x_test.append(test_data[:,:])
+    x_test = np.asarray(x_test).astype('float32')
+    #y_test = np.asarray(y_test).astype('float32')
+
+    x_test = np.reshape(x_test,(x_test.shape[0], x_test.shape[1],num_features))
 
 
-    #______reshape test-data to lstm expected shape_______
-
-    x_test = np.asarray(x_test)
-    y_test = np.asarray(y_test)
-
-    x_test = np.reshape(x_test,( x_test.shape[0], x_test.shape[1],num_features))
 
     #_________________________________________________________________________________________________________________________
 
@@ -644,7 +754,7 @@ def get_predictions(df):
         
         layer = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64, return_sequences=False))(inputs)
         
-        layer =  tf.keras.layers.Dense(32, kernel_initializer='lecun_normal', activation='selu')(layer)
+        layer =  tf.keras.layers.Dense(36, kernel_initializer='lecun_normal', activation='selu')(layer)
 
         layer =  tf.keras.layers.Dropout(0.4)(layer)
 
@@ -668,10 +778,10 @@ def get_predictions(df):
         
     model_dnn = build_model(in_window, out_window, num_features)
 
-    callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=7)
+    callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
 
     model_dnn.summary()
-    hist_simple = model_dnn.fit(x_train, y_train, epochs=80, batch_size=8, callbacks=[callback], shuffle=False, validation_data=(x_valid, y_valid))
+    hist_simple = model_dnn.fit(x_train, y_train, epochs=45, batch_size=8, callbacks=[callback], shuffle=False, validation_data=(x_valid, y_valid))
 
 
     y_pred = model_dnn.predict(x_test)
